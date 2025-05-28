66ea6ed7b15236bee5a9eb3613eaa5d005f9cfca
Wolker <contato@wolker.com.br>
2025-05-27 23:07:54 -0300

    Tweak google strategy settings

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  atlasUsername: process.env.ATLAS_USERNAME,
+  atlasPassword: process.env.ATLAS_PASSWORD,
+  mongoURI: process.env.MONGO_URI,
+  // mongoURI2:
+  //   process.env.MONGO_UR2,
+  cookieKey: process.env.COOKIE_KEY,
+};
